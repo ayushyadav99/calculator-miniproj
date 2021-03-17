e76bb2a4a689f32521f3cc24025705eb450548b8
Ayush Yadav <ayushyadav.ay42@gmail.com>
2021-03-17 11:40:39 +0530

    add logging using log4j

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8
-COPY ./target/calculator-miniproj-1.0-SNAPSHOT.jar ./
+COPY ./target/calculator-miniproj-1.0-SNAPSHOT-jar-with-dependencies.jar ./
 WORKDIR ./
-CMD ["java", "-cp", "calculator-miniproj-1.0-SNAPSHOT.jar", "App"]
+CMD ["java", "-cp", "calculator-miniproj-1.0-SNAPSHOT-jar-with-dependencies.jar", "App"]
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,37 @@
 import java.lang.Math;
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class App {
     public static void main(String[] args) {
-        for(int i=0; i<5; i++){
-            System.out.println("this works");
-        }
+        int run_flag = 1;
+        Scanner in = new Scanner(System.in);
+        do{
+            System.out.println("Enter the number corresponding to the operation you want to execute: ");
+            System.out.println("1. Square Root");
+            System.out.println("2. Exit");
+
+            int option = in.nextInt();
+            switch(option){
+                case 1:
+                    System.out.println("Input the operand");
+                    double operand = in.nextDouble();
+                    System.out.println("The Square root of " + operand + " is " + sqrt(operand));
+                    break;
+                case 2:
+                    run_flag = 0;
+                default:
+                    System.out.println("Invalid Command: Please Try Again");
+            }
+        }while(run_flag == 1);
     }
 
-    static double sqrt(double operant){
-        return Math.sqrt(operant);
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    static double sqrt(double operand){
+        logger.info("Applying square root function on operand : " + operand);
+        return Math.sqrt(operand);
     }
 }
