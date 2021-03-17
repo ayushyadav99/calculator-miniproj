725385c3084faebdd5bc4b50362e83731e5614fa
Ayush Yadav <ayushyadav.ay42@gmail.com>
2021-03-17 16:06:30 +0530

    add trueNegative test cases

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -45,5 +45,38 @@ public class AppTest {
         assertEquals(calculator.power(operandX, operandY), actual, 0.2f);
     }
 
+    @Test
+    public void trueNegativeSqrt(){
+        double operand = 9;
+        double actual = 4;
+//        System.out.println("Checking square root of 9");
+        assertNotEquals(calculator.sqrt(operand), actual, 0.2f);
+    }
+
+    @Test
+    public void trueNegativeFactorial(){
+        int operand = 6;
+        int actual = 722;
+//        System.out.println("Checking square root of 9");
+        assertNotEquals(calculator.factorial(operand), actual, 0.2f);
+    }
+
+    @Test
+    public void trueNegativeNLog(){
+        double operand = 10;
+        double actual = 2;
+//        System.out.println("Checking square root of 9");
+        assertNotEquals(calculator.nLog(operand), actual, 0.2f);
+    }
+
+    @Test
+    public void trueNegativePower(){
+        double operandX = 5;
+        double operandY = 3;
+        double actual = 132.651;
+//        System.out.println("Checking square root of 9");
+        assertNotEquals(calculator.power(operandX, operandY), actual, 0.2f);
+    }
+
 }
 
